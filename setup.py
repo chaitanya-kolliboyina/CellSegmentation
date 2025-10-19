cd902f06e2c0ac58a179c14276d63c167f8bec33
chaitanya-kolliboyina <kvschaitanya17@gmail.com>
2025-10-19 10:51:07 +0530

    setup.py added

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cell_segmentation',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'transformers',
+        'fastapi',
+        'uvicorn',
+        'pillow',
+        'scikit-learn',
+        'numpy',
+    ],
+    description='Library for cancer cell segmentation from MRI images',
+)
